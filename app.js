bdb285cc264c913da138650166e3b8b9fb860d16
Clemens Krasser <if25b179@technikum-wien.at>
2026-03-19 09:52:31 +0100

    Api endpoint mit error handling. /queue/register

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const express = require('express')
+import {addToQueue } from './queue.js';
+import express from 'express';
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/queue/register', (req, res) => {
+
+    try {
+        const entry = addToQueue();
+        res.status(200);
+        res.send(entry);
+    } catch (e) {
+        console.error(e);
+        res.status(500);
+        res.send();
+    }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
