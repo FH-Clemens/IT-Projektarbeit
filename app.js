b670f18f07b0aa79f787d2d35f1902c7b5e1c71b
Ayat Husein <ayayt234@gmail.com>
2026-04-01 12:48:19 +0200

    cleanup code

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import { readFile } from 'node:fs/promises';
 
 import StartupManager from "./src/startup.js";
 import { addToQueue, getQueue } from './src/queue.js';
-import { saveQueue } from './src/queuePersistence.js';
+import * as fs from 'fs/promises';
+
 
 
 const app = express()
@@ -45,27 +46,37 @@ async function start() {
 
     try {
         const configPath = __APP_DIR__ + "/config.json";
-        const response = await readFile(configPath);
+        const response = await readFile(configPath, 'utf-8');
 
         config = JSON.parse(response);
     } catch (e) {
         console.error('Error reading config file: ', e);
     }
-
     const startupManager = new StartupManager();
     startupManager.addHook(async (properties, out) => {
         try {
-            let queue = getQueue();
+            const retainDays = properties["retain-data-days"] || 3;
+            const queueDataPath = path.join(__APP_DIR__, "queue-data");
 
-            const now = Date.now();
-            const maxAge = properties["retain-data-days"] * 24 * 60 * 60 * 1000;
+            const cutoffDate = new Date();
+            cutoffDate.setDate(cutoffDate.getDate() - retainDays);
 
-            queue = queue.filter(item => {
-                const createdAt = new Date(item.createdAt).getTime();
-                return (now - createdAt) <= maxAge;
-            });
+            const year = cutoffDate.getFullYear();
+            const month = String(cutoffDate.getMonth() + 1).padStart(2, "0");
+            const day = String(cutoffDate.getDate()).padStart(2, "0");
+            const threshold = `${year}${month}${day}`;
 
-            saveQueue(queue);
+            const entries = await fs.readdir(queueDataPath);
+
+            for (const folderName of entries) {
+                if (/^\d{8}$/.test(folderName)) {
+                    if (folderName < threshold) {
+                        const fullPath = path.join(queueDataPath, folderName);
+                        await fs.rm(fullPath, { recursive: true, force: true });
+                        console.log("Deleted:", folderName);
+                    }
+                }
+            }
 
             out.sequential = true;
 
@@ -77,6 +88,8 @@ async function start() {
     console.log('Starting server...');
 
     await startupManager.run(config);
+
+
 
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
